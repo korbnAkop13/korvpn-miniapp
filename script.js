1962f855ccc926efcce7a7ca16798f8b83a2b9a2
korbnAkop13 <koryun.work@yandex.ru>
2026-01-31 01:49:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const copyBtn = document.getElementById("copyBtn");
 
 // ТУТ ХРАНИТСЯ ПОЛНЫЙ КЛЮЧ
-const FULL_KEY = "ss://VERY-LONG-OUTLINE-KEY-GOES-HERE";
+const FULL_KEY = "ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTp2U1NkcU55SWhjNWhYcUJjcUt2NTgz@194.147.33.147:58652/?outline=1";
 
 copyBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(FULL_KEY);
